0add8950b2c296defd50242c2ab880b81831b06b
hannah68 <reihaneh.naderi1989@gmail.com>
2023-01-31 11:36:49 +0000

    Create general components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FeatureStoriesComponent } from './feature-stories/feature-stories.component';
+import { RecentStoriesComponent } from './recent-stories/recent-stories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    FeatureStoriesComponent,
+    RecentStoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feature-stories/feature-stories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-stories',
+  templateUrl: './feature-stories.component.html',
+  styleUrls: ['./feature-stories.component.css']
+})
+export class FeatureStoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
